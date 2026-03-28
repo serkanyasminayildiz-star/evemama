bff808eddd1220d9df1b2148dbaf9a8c239628e5
serkanyasminayildiz-star <serkanyasminayildiz@gmail.com>
2026-03-28 11:34:05 +0200

    Update route.ts
    sonuc route guncellendi

--- app/api/odeme/sonuc/route.ts
+++ app/api/odeme/sonuc/route.ts
@@ -34,6 +34,6 @@ export async function POST(req: NextRequest) {
     const data = await response.json();
     return NextResponse.json(data);
   } catch (err: any) {
-    return NextResponse.json({ error: err.message }, { status: 300 });
+    return NextResponse.json({ error: err.message }, { status: 500 });
   }
 }
